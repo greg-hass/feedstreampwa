cdbe9db379016246a0ce578e3e9406a85a3f9de7
Greg <galcorn@icloud.com>
2026-01-16 09:37:32 +0000

    Work around SW build crash

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -41,6 +41,8 @@ export default defineConfig({
 				}
 			},
 			workbox: {
+				mode: 'development',
+				disableDevLogs: true,
 				runtimeCaching: [
 					{
 						urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
